edcfedc71c66abbc1e8e4065c2d16cd7f8b6da26
SUBHANKAR SAHOO <subhankarsahoo820@gmail.com>
2024-12-30 01:19:49 +0530

    Add files via upload

---
+++ Methods_and_Operations_/Concatenate.java
@@ -0,0 +1,9 @@
+public class Concatenate 
+{
+    public static void main(String[] args) {
+        String name1 = "Subhankar";
+        String name2 = "sahoo";
+        String name3 = name1 +" "+ name2; // Concatenate strings
+        System.out.println(name3);   // Print the concatenated result
+    }
+}
