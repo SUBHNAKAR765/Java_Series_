c94fca1da815285d5e023dea32327689e4db098d
SUBHANKAR SAHOO <subhankarsahoo820@gmail.com>
2024-12-30 01:20:38 +0530

    Add files via upload

---
+++ Methods_and_Operations_/replace.java
@@ -0,0 +1,8 @@
+public class replace {
+    public static void main(String[] args) {
+        String s1 = "subhankar sahoo";
+        String s2 = s1.replace('s' , 'S');
+        System.out.println(s2);
+
+    }
+}
