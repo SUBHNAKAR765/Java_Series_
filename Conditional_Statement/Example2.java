d43ccc0acbc67681cc21fe6f5e101a64ebedbccc
SUBHANKAR SAHOO <subhankarsahoo820@gmail.com>
2024-12-30 10:25:57 +0530

    Add files via upload

---
+++ Conditional_Statement/Example2.java
@@ -0,0 +1,12 @@
+
+public class Example2 {
+    public static void main(String[] args) {
+        
+        boolean isSunup= true;
+        if(isSunup=true)
+        System.out.println("Day");
+        else
+        System.out.println("Night");
+    
+}
+}
